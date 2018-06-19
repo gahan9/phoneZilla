c36773f96e122111d53fce0dc7e4114f561aad6e
gahan9 <gahansaraiya@gmail.com>
2018-06-19 10:10:20 +0530

    added context processor and imei field

--- inventory_management/context_processors.py
+++ inventory_management/context_processors.py
@@ -1,12 +1,20 @@
 # coding=utf-8
 __author__ = "Gahan Saraiya"
-from core_settings.settings import COMPANY_TITLE, COMPANY_LOGO, PRODUCT_TYPE, COPYRIGHT_SINCE
+from core_settings import settings
 
 
 def site_details(request):
     return {
-        "SITE_NAME": COMPANY_TITLE,
-        "PRODUCT_TYPE": PRODUCT_TYPE,
-        "SITE_LOGO": COMPANY_LOGO,
-        "COPYRIGHT_SINCE": COPYRIGHT_SINCE
+        "SITE_NAME": settings.COMPANY_TITLE,
+        "SITE_LOGO": settings.COMPANY_LOGO,
+        "COMPANY_EMAIL": settings.COMPANY_EMAIL,
+        "COMPANY_CONTACT_NUMBER": settings.COMPANY_CONTACT_NUMBER,
+        "COMPANY_ADDRESS_LINE_ONE": settings.COMPANY_ADDRESS_LINE_ONE,
+        "COMPANY_ADDRESS_LINE_TWO": settings.COMPANY_ADDRESS_LINE_TWO,
+        "COMPANY_COUNTRY": settings.COMPANY_COUNTRY,
+        "COMPANY_WEBSITE": settings.COMPANY_WEBSITE,
+        "INV_CURRENCY_SYMBOL": settings.INV_CURRENCY_SYMBOL,
+        "INV_CURRENCY_PREFIX": settings.INV_CURRENCY_PREFIX,
+        "PRODUCT_TYPE": settings.PRODUCT_TYPE,
+        "COPYRIGHT_SINCE": settings.COPYRIGHT_SINCE
     }
--- main/apps.py
+++ main/apps.py
@@ -1,12 +1,7 @@
 # coding=utf-8
 __author__ = "Gahan Saraiya"
 from django.apps import AppConfig
-from suit.apps import DjangoSuitConfig
 
 
 class MainConfig(AppConfig):
     name = 'main'
-
-
-class SuitConfig(DjangoSuitConfig):
-    layout = 'vertical'
